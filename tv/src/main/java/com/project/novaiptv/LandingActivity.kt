8c370a13eadae7a996f5deee31aa1146eb07b2f3
Amir5470 <106452515+Amir5470@users.noreply.github.com>
2025-09-09 20:52:09 -0400

    initial commit

--- tv/src/main/java/com/project/novaiptv/LandingActivity.kt
+++ tv/src/main/java/com/project/novaiptv/LandingActivity.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.project.novaiptv.databinding.LandingPageBinding // Assuming ViewBinding
-import android.widget.button
+import android.widget.Button
 
 class LandingActivity : AppCompatActivity() {
 
